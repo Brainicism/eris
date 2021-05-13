8bd5bf2a90754f4a2fca76b88812b2222beef5aa
Brian Le <brainicism@gmail.com>
2021-05-12 21:54:36 -0400

    Use correct defualt value for opusPassthrough

--- index.d.ts
+++ index.d.ts
@@ -1215,6 +1215,7 @@ declare namespace Eris {
     pcmSize?: number;
     samplingRate?: number;
     voiceDataTimeout?: number;
+    opusPassthrough?: boolean;
   }
   interface VoiceServerUpdateData extends Omit<VoiceConnectData, "channel_id"> {
     guild_id: string;
