543214c5edf6d8bd3ea9cae198151f3b3c2c29cf
Donovan Daniels <hewwo@yiff.rocks>
2021-08-06 12:27:21 -0500

    add components to interaction pick

--- index.d.ts
+++ index.d.ts
@@ -92,8 +92,8 @@ declare namespace Eris {
     data?: InteractionContent;
   };
 
-  type InteractionContent = Pick<WebhookPayload, "content" | "embeds" | "allowedMentions" | "tts" | "flags">;
-  type InteractionWebhookContent = Pick<WebhookPayload, "content" | "embeds" | "file" | "allowedMentions" | "tts" | "flags">;
+  type InteractionContent = Pick<WebhookPayload, "content" | "embeds" | "allowedMentions" | "tts" | "flags" | "components">;
+  type InteractionWebhookContent = Pick<WebhookPayload, "content" | "embeds" | "file" | "allowedMentions" | "tts" | "flags" | "components">;
 
   //Slash Commands
   type SlashCommandOptions = {
