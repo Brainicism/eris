762ffe235d9d907689f359593be76f770d37f771
Brian Le <brainicism@gmail.com>
2023-03-03 23:02:43 +0000

    Remove deprecated and conflicting function signature

--- index.d.ts
+++ index.d.ts
@@ -7,9 +7,6 @@ import { URL } from "url";
 import { Socket as DgramSocket } from "dgram";
 import * as WebSocket from "ws";
 
-/** @deprecated */
-declare function Dysnomia(token: string, options?: Dysnomia.ClientOptions): Dysnomia.Client;
-
 declare namespace Dysnomia {
   export const Constants: Constants;
   export const VERSION: string;
